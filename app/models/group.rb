89c9b22436e137fd9e3c75dd542458195a8aa5b5
钟道有 <13657078490@163.com>
2016-12-25 21:00:46 +0800

    implement group relation

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,5 +3,7 @@ class Group < ActiveRecord::Base
   has_many :posts
   validates :title, presence: true
 
+  has_many :group_relationships
+  has_many :members, through: :group_relationships, source: :user
 
 end
